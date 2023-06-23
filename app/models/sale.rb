f9c0bfcfdbe98df68437b95f7720107850b60a4a
Felipe Soller <soller@outlook.com>
2023-06-23 13:43:45 -0300

    refactor profit or loss calculation

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,8 +3,8 @@ class Sale < ApplicationRecord
 
   def self.profit_or_loss_by_month(year)
     joins(:stock)
-      .where('extract(year from sold_at) = ?', year)
-      .group('extract(month from sold_at)')
-      .select('extract(month from sold_at) as month, sum(quantity * stocks.price) as total')
+      .where('extract(year from sales.sold_at) = ?', year)
+      .group('extract(month from sales.sold_at)')
+      .select('extract(month from sales.sold_at) as month, sum(sales.quantity * (sales.price - stocks.price)) as total')
   end
 end
