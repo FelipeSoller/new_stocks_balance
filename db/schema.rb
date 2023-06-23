6c41715f6d6ee4f9325723ac8a9f38f10a3c0c88
Felipe Soller <soller@outlook.com>
2023-06-23 13:42:39 -0300

    adds price to sales model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_160634) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_163423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_160634) do
     t.date "sold_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price"
     t.index ["stock_id"], name: "index_sales_on_stock_id"
   end
 
