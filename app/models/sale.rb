d20763e15ddc8d6968496583fbd2270051f22b0e
Felipe Soller <soller@outlook.com>
2023-06-23 16:27:14 -0300

    refactor to use sale profit or loss in calculations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -5,6 +5,6 @@ class Sale < ApplicationRecord
     joins(:stock)
       .where('extract(year from sales.sold_at) = ?', year)
       .group('extract(month from sales.sold_at)')
-      .select('extract(month from sales.sold_at) as month, sum(sales.quantity * (sales.price - stocks.price)) as total')
+      .select('extract(month from sales.sold_at) as month, sum(sales.profit_or_loss) as total')
   end
 end
