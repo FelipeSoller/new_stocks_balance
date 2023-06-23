220fd6f9f8693a4198652e1bb7bcabfc4565112a
Felipe Soller <soller@outlook.com>
2023-06-23 13:17:06 -0300

    adds profit or loss calculation in sale model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,10 @@
 class Sale < ApplicationRecord
   belongs_to :stock
+
+  def self.profit_or_loss_by_month(year)
+    joins(:stock)
+      .where('extract(year from sold_at) = ?', year)
+      .group('extract(month from sold_at)')
+      .select('extract(month from sold_at) as month, sum(quantity * stocks.price) as total')
+  end
 end
