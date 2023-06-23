412742c7544a658f5bf15a41e7e2c210f103b782
Felipe Soller <soller@outlook.com>
2023-06-23 13:17:32 -0300

    updates app routes to add reports

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :items
   resources :sales
 
+  get 'reports/monthly_profit_or_loss', to: 'reports#monthly_profit_or_loss', as: 'reports_monthly_profit_or_loss'
+
   root to: 'items#index'
 end
