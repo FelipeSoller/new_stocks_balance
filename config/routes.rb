3f16810c0debf89376d488056dbb8805cb81bad1
Felipe Soller <soller@outlook.com>
2023-06-23 15:28:04 -0300

    updates app routes to add purchases

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :stocks
   resources :sales
+  resources :purchases, only: [:new, :create]
 
   get 'reports/monthly_profit_or_loss', to: 'reports#monthly_profit_or_loss', as: 'reports_monthly_profit_or_loss'
 
