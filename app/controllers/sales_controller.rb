4b9b5523f767326485e31169d7a7fac67e32b696
Felipe Soller <soller@outlook.com>
2023-06-23 13:09:43 -0300

    adds create and read in SalesController

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,7 +1,24 @@
 class SalesController < ApplicationController
   def index
+    @sales = Sale.all
   end
 
   def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+    if @sale.save
+      redirect_to sales_path, notice: 'Sale created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sale_params
+    params.require(:sale).permit(:item_id, :quantity, :sold_at)
   end
 end
