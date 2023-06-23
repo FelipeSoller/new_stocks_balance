74bfdca71c5c98bee57f6ad37c5e552c2508ecb6
Felipe Soller <soller@outlook.com>
2023-06-23 13:09:17 -0300

    adds create and read in StocksController

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,24 @@
 class StocksController < ApplicationController
   def index
+    @stocks = Item.all
   end
 
   def new
+    @stock = Item.new
+  end
+
+  def create
+    @stock = Item.new(stock_params)
+    if @stock.save
+      redirect_to stocks_path, notice: 'Item created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:name, :quantity, :price)
   end
 end
