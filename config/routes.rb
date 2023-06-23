0f847e85ab095ebe9fff81d96e2a7628da019f48
Felipe Soller <soller@outlook.com>
2023-06-23 13:07:46 -0300

    generates stocks and sales controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sales/index'
+  get 'sales/new'
+  get 'stocks/index'
+  get 'stocks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
