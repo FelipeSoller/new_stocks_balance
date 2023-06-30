c73fce8255f3c8a960daf0966fca62267af7bf63
Felipe Soller <felipe.souza@imaginedone.com.br>
2023-06-30 10:40:06 -0300

    fix profit or loss calculation

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,7 +4,7 @@ class Sale < ApplicationRecord
   def self.profit_or_loss_by_month(year)
     joins(:stock)
       .where('extract(year from sales.sold_at) = ?', year)
-      .group('extract(month from sales.sold_at)')
-      .select('extract(month from sales.sold_at) as month, sum(sales.profit_or_loss) as total, ROUND((sum(sales.profit_or_loss) / sum(stocks.price) * 100), 2) as percentage')
+      .group('extract(month from sales.sold_at), sales.quantity, stocks.price')
+      .select('extract(month from sales.sold_at) as month, sum(sales.profit_or_loss) as total, ROUND((sum(sales.profit_or_loss) / (sales.quantity * stocks.price) * 100), 2) as percentage')
   end
 end
